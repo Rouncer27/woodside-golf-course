f95bc81682afc84a8224d66bf96e334d3de951af
Trevor Rounce <trevor@switchbackcreative.ca>
2015-12-05 11:33:21 -0700

    Locking out the cart and checkout pages

--- wp-content/themes/SWBtheme/page-checkout.php
+++ wp-content/themes/SWBtheme/page-checkout.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 	<main class="checkoutpage">
+	<?php if ( is_user_logged_in() ) { ?>
 	<?php if(have_posts() ) : while(have_posts() ) : the_post(); ?> 
 		<section class="checkoutview">
 			<div class="checkoutview__container">
@@ -9,5 +10,11 @@
 			</div>
 		</section>
 	<?php endwhile; endif; ?>
+	<div class="under_contruction">
+		<h2>Woodside Store</h2>
+		<p>Coming Soon</p>
+	</div>
+
+	<?php } ?>
 	</main>
 <?php get_footer(); ?>
