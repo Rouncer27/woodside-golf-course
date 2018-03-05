e92b0b2198d3b93902b1a7603b0be3c8eedb3c6b
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2018-03-05 15:05:43 -0700

    league pdf

--- wp-content/themes/SWBtheme/page-leagues.php
+++ wp-content/themes/SWBtheme/page-leagues.php
@@ -26,7 +26,7 @@
                     <div class="swbleagues__item--content">
                         <p>Senior Men and Women ages 55+ are invited to join us for 18 holes of play each Monday morning for a 9:00am modified shotgun start.  This league features both individual and team handicapped games each week that are designed to be relaxed, fun and provide  some friendly competition.  In addition, there are a number of special event days throughout the season, such as our annual Steak and Beans Team Event, where the winning team eats steak and losing team eats beans.  The game of golf provides us so many opportunities to create lasting impressions - join the Monday Morning Mixed Senior League in 2018 to be apart of some exceptional moments.</p>
                         <div class="buttons">
-                            <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/leagues/League-Information.pdf">League Information</a>
+                            <a target="_blank" href="<?php echo esc_url( get_post_meta( get_the_id(), '_swb_league_information', true ) ); ?>">League Information</a>
                             <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/leagues/League-Information.pdf">Rate Sheet</a>
                             <a target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/leagues/League-Information.pdf">Standings</a>
 						</div>
