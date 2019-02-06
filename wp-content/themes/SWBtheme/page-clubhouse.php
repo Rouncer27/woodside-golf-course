4b6e5aa3168c73fb79478a6ffcbd5c9ffb454bdd
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2019-02-06 08:56:50 -0700

    clubhouse page, now the dinning option on the main menu, swapped the woods banner with the meeting rooms section

--- wp-content/themes/SWBtheme/page-clubhouse.php
+++ wp-content/themes/SWBtheme/page-clubhouse.php
@@ -2,6 +2,7 @@
 		<main <?php post_class( 'clubhouse' ); ?>>
 			<?php the_post(); ?>
 			<article>
+				
 				<header class="clubhouseheader">
 					<img src="<?php echo get_template_directory_uri(); ?>/img/clubhouse/circletree.png" alt="Woodside logo with crazy colourful background">
 					<h1><?php the_title(); ?></h1>
@@ -10,18 +11,14 @@
 				<div class="slogan">
 					<?php get_template_part( 'content/content', 'quote' ); ?>
 				</div>
-				<section class="meetingrooms">
-					<div class="meetingrooms__container">
-						<div class="meetingrooms__container--header">
-							<h2>Meeting Rooms</h2>
-							<p>Our meeting room provides flexibility for any number of events</p>
-						</div>
-						<div class="meetingrooms__container--content">
-							<p><?php the_field( 'meeting_rooms'); ?></p>
-							<a href="<?php echo home_url(); ?>/contact">Book A Room</a>
-						</div>
+				<section class="woodsrestaurant">
+					<div class="woodsrestaurant__container">
+						<h3><span>The Woods</span> restaurant</h3>
+						<p><?php the_field( 'woods' ); ?></p>
+						<a target="_blank" title="Click to go to the Woodside Restaurant" href="http://www.thewoodsrestaurant.ca">Full Site</a>
 					</div>
 				</section>
+				
 				<section class="patio">
 					<div class="patio__container">
 						<div class="patio__container--content">
@@ -36,13 +33,20 @@
 						</div>
 					</div>
 				</section>
-				<section class="woodsrestaurant">
-					<div class="woodsrestaurant__container">
-						<h3><span>The Woods</span> restaurant</h3>
-						<p><?php the_field( 'woods' ); ?></p>
-						<a target="_blank" title="Click to go to the Woodside Restaurant" href="http://www.thewoodsrestaurant.ca">Full Site</a>
+
+				<section class="meetingrooms">
+					<div class="meetingrooms__container">
+						<div class="meetingrooms__container--header">
+							<h2>Meeting Rooms</h2>
+							<p>Our meeting room provides flexibility for any number of events</p>
+						</div>
+						<div class="meetingrooms__container--content">
+							<p><?php the_field( 'meeting_rooms'); ?></p>
+							<a href="<?php echo home_url(); ?>/contact">Book A Room</a>
+						</div>
 					</div>
 				</section>
+				
 			</article>
 		</main>
 <?php get_footer(); ?>
